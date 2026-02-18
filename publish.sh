0fd86a2672a8752c02c994a785a037b3f5a1e8fd
Connor Makowski <connorman528@gmail.com>
2026-02-18 11:27:35 -0500

    Release stable 1.1.0 CPP based geokdtree.

--- publish.sh
+++ publish.sh
@@ -1,2 +1,4 @@
 python -m build
-python -m twine upload dist/*
+# Remove all whl builds in the dist folder as we require building the c++ extension on target machines.
+rm -r dist/*.whl
+python -m twine upload dist/*.tar.gz
